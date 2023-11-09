15b010090930361e78009d0d0a6b1c8889418fd0
leanhnhan147 <anhnhan9a3@gmail.com>
2023-11-10 02:35:27 +0700

    add configure in web mvc config to convert between objects and json/xml format

--- src/main/java/com/ecommerce/backend/constant/Constant.java
+++ src/main/java/com/ecommerce/backend/constant/Constant.java
@@ -2,6 +2,9 @@ package com.ecommerce.backend.constant;
 
 public class Constant {
 
+    public static final String DATE_FORMAT = "dd/MM/yyyy";
+    public static final String DATE_TIME_FORMAT = "dd/MM/yyyy HH:mm:ss";
+
     public static final String MEDIA_RESOURCE_TYPE_IMAGE = "image";
 
     public static final Integer MEDIA_RESOURCE_KIND_IMAGE = 1;
