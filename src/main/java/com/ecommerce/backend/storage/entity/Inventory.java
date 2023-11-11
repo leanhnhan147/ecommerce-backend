d384161f220ca1754599995a834f21e3077f7231
leanhnhan147 <anhnhan9a3@gmail.com>
2023-11-11 22:00:44 +0700

    migrate database

---
+++ src/main/java/com/ecommerce/backend/storage/entity/Inventory.java
@@ -0,0 +1,34 @@
+package com.ecommerce.backend.storage.entity;
+
+import com.ecommerce.backend.storage.base.Auditable;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "inventory")
+public class Inventory extends Auditable<String> {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Double originalPrice;
+    private Integer quantity;
+    private Date importTime;
+
+    @ManyToOne
+    @JoinColumn(name = "product_variation_id")
+    private ProductVariation productVariation;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+}
