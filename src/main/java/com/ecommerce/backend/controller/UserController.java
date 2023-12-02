41c1501ef259e7bb8d3648c3ddbb69f73245ea61
leanhnhan147 <anhnhan9a3@gmail.com>
2023-12-02 17:46:33 +0700

    fix api update profile user, add api delete user

--- src/main/java/com/ecommerce/backend/controller/UserController.java
+++ src/main/java/com/ecommerce/backend/controller/UserController.java
@@ -3,6 +3,7 @@ package com.ecommerce.backend.controller;
 import com.ecommerce.backend.dto.ApiMessageDto;
 import com.ecommerce.backend.dto.ResponseListDto;
 import com.ecommerce.backend.dto.account.LoginAuthDto;
+import com.ecommerce.backend.dto.customer.CustomerDto;
 import com.ecommerce.backend.dto.user.UserAdminDto;
 import com.ecommerce.backend.dto.user.UserDto;
 import com.ecommerce.backend.form.login.LoginForm;
@@ -10,6 +11,7 @@ import com.ecommerce.backend.form.user.CreateUserForm;
 import com.ecommerce.backend.form.user.UpdateProfileUserForm;
 import com.ecommerce.backend.form.user.UpdateUserForm;
 import com.ecommerce.backend.service.UserService;
+import com.ecommerce.backend.storage.criteria.CustomerCriteria;
 import com.ecommerce.backend.storage.criteria.UserCriteria;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -47,6 +49,14 @@ public class UserController extends BasicController{
         return responseListDtoApiMessageDto;
     }
 
+    @GetMapping(value = "/auto-complete", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ApiMessageDto<List<UserDto>> getListAutoComplete(UserCriteria userCriteria) {
+        ApiMessageDto<List<UserDto>> apiMessageDto = new ApiMessageDto<>();
+        apiMessageDto.setData(userService.getUserListAutoComplete(userCriteria));
+        apiMessageDto.setMessage("Get list auto complete success");
+        return apiMessageDto;
+    }
+
     @PostMapping(value = "/create", produces = MediaType.APPLICATION_JSON_VALUE)
     public ApiMessageDto<String> create(@Valid @RequestBody CreateUserForm createUserForm, BindingResult bindingResult) {
         ApiMessageDto<String> apiMessageDto = new ApiMessageDto<>();
@@ -60,6 +70,14 @@ public class UserController extends BasicController{
         ApiMessageDto<String> apiMessageDto = new ApiMessageDto<>();
         userService.updateUser(updateUserForm);
         apiMessageDto.setMessage("Update user success");
+        return apiMessageDto;
+    }
+
+    @DeleteMapping(value = "/delete/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ApiMessageDto<String> delete(@PathVariable("id") Long id) {
+        ApiMessageDto<String> apiMessageDto = new ApiMessageDto<>();
+        userService.deleteUser(id);
+        apiMessageDto.setMessage("Delete user success");
         return apiMessageDto;
     }
 
@@ -79,13 +97,11 @@ public class UserController extends BasicController{
         return apiMessageDto;
     }
 
-//    @PutMapping(value = "/update-profile", produces = MediaType.APPLICATION_JSON_VALUE)
-//    public ApiMessageDto<String> updateProfile(@AuthenticationPrincipal CustomUserDetails userDetails,
-//                                               @Valid @RequestBody UpdateProfileUserForm updateProfileUserForm,
-//                                               BindingResult bindingResult) {
-//        ApiMessageDto<String> apiMessageDto = new ApiMessageDto<>();
-//        userService.updateProfile(userDetails.getUser().getId(), updateProfileUserForm);
-//        apiMessageDto.setMessage("Update profile success");
-//        return apiMessageDto;
-//    }
+    @PutMapping(value = "/update-profile", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ApiMessageDto<String> updateProfile(@Valid @RequestBody UpdateProfileUserForm updateProfileUserForm, BindingResult bindingResult) {
+        ApiMessageDto<String> apiMessageDto = new ApiMessageDto<>();
+        userService.updateProfile(getCurrentUser(), updateProfileUserForm);
+        apiMessageDto.setMessage("Update profile success");
+        return apiMessageDto;
+    }
 }
--- src/main/java/com/ecommerce/backend/form/user/UpdateProfileUserForm.java
+++ src/main/java/com/ecommerce/backend/form/user/UpdateProfileUserForm.java
@@ -3,6 +3,7 @@ package com.ecommerce.backend.form.user;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
+import javax.validation.constraints.Email;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.Past;
 import java.util.Date;
@@ -19,6 +20,15 @@ public class UpdateProfileUserForm {
     @ApiModelProperty(name = "birthday", required = true)
     @Past(message = "date of birth must be in the past")
     private Date birhday;
+
+    @NotEmpty(message = "phone cannot be null")
+    @ApiModelProperty(name = "phone", required = true)
+    private String phone;
+
+    @Email
+    @NotEmpty(message = "email cannot be null")
+    @ApiModelProperty(name = "email", required = true)
+    private String email;
 
     @NotEmpty(message = "oldPassword cannot be null")
     @ApiModelProperty(required = true)
