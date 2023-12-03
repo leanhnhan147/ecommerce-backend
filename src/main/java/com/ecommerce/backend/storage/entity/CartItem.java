7ae48d6954c18f99b06a6a57f27b000d5cd7b640
leanhnhan147 <anhnhan9a3@gmail.com>
2023-12-04 00:56:33 +0700

    migrate database add cart item entity

---
+++ src/main/java/com/ecommerce/backend/storage/entity/CartItem.java
@@ -0,0 +1,31 @@
+package com.ecommerce.backend.storage.entity;
+
+import com.ecommerce.backend.storage.base.Auditable;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "cart_item")
+public class CartItem extends Auditable<String> {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Integer quantity;
+
+    @ManyToOne
+    @JoinColumn(name = "product_variation_id")
+    private ProductVariation productVariation;
+
+    @ManyToOne
+    @JoinColumn(name = "customer_id")
+    private Customer customer;
+}
