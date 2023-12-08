92392dbf593d14054bd4f5a822f9169f052d9d92
leanhnhan147 <anhnhan9a3@gmail.com>
2023-12-08 16:26:34 +0700

    migrate database add order, order detail, order tracking entity

---
+++ src/main/java/com/ecommerce/backend/storage/entity/OrderTracking.java
@@ -0,0 +1,48 @@
+package com.ecommerce.backend.storage.entity;
+
+import com.ecommerce.backend.storage.base.Auditable;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@EntityListeners(AuditingEntityListener.class)
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "order_tracking")
+public class OrderTracking {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+
+    @Column(columnDefinition = "text")
+    private String note;
+
+    private Integer state;
+
+    @CreatedDate
+    @Column(name = "created_date", nullable = false, updatable = false)
+    private Date createdDate;
+
+    @ManyToOne
+    @JoinColumn(name = "order_id")
+    private Order order;
+
+    @ManyToOne
+    @JoinColumn(name = "customer_id")
+    private Customer customer;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+}
